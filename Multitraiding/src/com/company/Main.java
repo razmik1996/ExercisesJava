3fe1220762a9fdb7b84d06328a9f49b8a6f98a45
razmik1996 <razfreelancer@mail.ru>
2019-08-11 16:36:48 +0400

    Multithraiding

--- Multitraiding/src/com/company/Main.java
+++ Multitraiding/src/com/company/Main.java
@@ -2,6 +2,21 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
+        Thread t = Thread.currentThread();
 
+        System.out.println(t);
+
+        t.setName("Mine");
+        System.out.println(t);
+
+        try {
+            for(int i = 0; i < 5; i++) {
+                System.out.println(i);
+                Thread.sleep(1000);
+            }
+        }
+        catch (InterruptedException e){
+            System.out.println("END");
+        }
     }
 }
